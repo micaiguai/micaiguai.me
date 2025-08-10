7ba12bac0f9eaea348af6a34b56441965d200b38
micaiguai <1399880823@qq.com>
2025-08-11 00:40:56 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="text-gray-600 h-dvh dark:text-gray-300 dark:bg-black">
     <Navbar />
-    <main class="mb-20">
+    <main class="mb-20 px-4">
       <RouterView />
     </main>
-    <Footer class="content m-auto" />
+    <Footer class="content m-auto px-4" />
   </div>
 </template>
