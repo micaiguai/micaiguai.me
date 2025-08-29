2cdfbaa67c2fc0cf9f71c31b40f328c3f145dbc3
micaiguai <1399880823@qq.com>
2025-08-29 11:11:25 +0800

    chore: update

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <img class="rounded-full size-32 select-none ring ring-gray-300" src="https://raw.githubusercontent.com/micaiguai/assets/refs/heads/main/images/avatar.webp">
+  <img
+    class="image-render-pixel rounded-full size-32 select-none ring ring-gray-300"
+    src="https://raw.githubusercontent.com/micaiguai/assets/refs/heads/main/images/avatar.png"
+  >
 </template>
